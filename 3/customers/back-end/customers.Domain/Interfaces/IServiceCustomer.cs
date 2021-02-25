d8dab8969b89ebe8c3ce01f72cf90518555736bb
Guilherme Francisco <guilherme.francis2017@gmail.com>
2021-02-24 21:46:20 -0300

    Criação de interface de serviço do cliente e seu respectivo serviço utilizando a interface.

---
+++ 3/customers/back-end/customers.Domain/Interfaces/IServiceCustomer.cs
@@ -0,0 +1,14 @@
+﻿using customers.Domain.DTOs;
+using System.Collections.Generic;
+
+namespace customers.Domain.Interfaces
+{
+    public interface IServiceCustomer
+    {
+        CustomerDTO Insert(CreateCustomerDTO createCustomer);
+        CustomerDTO Update(UpdateCustomerDTO updateCustomer);
+        void Delete(int id);
+        IEnumerable<CustomerDTO> RecoverAll();
+        CustomerDTO RecoverById(int id);
+    }
+}
---
+++ 3/customers/back-end/customers.Services/Services/CustomerService.cs
@@ -0,0 +1,38 @@
+﻿using customers.Domain.DTOs;
+using customers.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace customers.Service.Services
+{
+    public class CustomerService : IServiceCustomer
+    {
+        private readonly IRepositoryCustomer _repositoryCustomer;
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerDTO Insert(CreateCustomerDTO createCustomer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CustomerDTO> RecoverAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerDTO RecoverById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerDTO Update(UpdateCustomerDTO updateCustomer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
