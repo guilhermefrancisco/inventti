78a416cc104697a0f0a285fc8e73731497266e95
Guilherme Francisco <guilherme.francis2017@gmail.com>
2021-02-24 21:28:46 -0300

    Adicionado classe CustomerRepository, onde utiliza a baseRepository e seus métodos para manipulação dos dados no contexto. Realizado a implementação da interface de repositório do customer. Nesta classe é possível realizar a adição de regras específicas do cliente caso necessário.

---
+++ 3/customers/back-end/customers.Infra.Data/Repository/CustomerRepository.cs
@@ -0,0 +1,23 @@
+﻿using customers.Domain.Entities;
+using customers.Domain.Interfaces;
+using customers.Infra.Data.Context;
+using System.Collections.Generic;
+
+namespace customers.Infra.Data.Repository
+{
+    public class CustomerRepository : BaseRepository<Customer, int>, IRepositoryCustomer
+    {
+        public CustomerRepository(CustomersContext customersContext) : base(customersContext) { }
+        public IList<Customer> GetAll() => base.Select();
+        public Customer GetById(int id) => base.Select(id);
+        public void Remove(int id) => base.Delete(id);
+
+        public void Save(Customer customer)
+        {
+            if (customer.Id == 0)
+                base.Insert(customer);
+            else
+                base.Update(customer);
+        }
+    }
+}
