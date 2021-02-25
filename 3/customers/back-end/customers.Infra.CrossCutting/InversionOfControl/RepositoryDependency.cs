51bdb728686f5e9ec1e8097c9c90a63c858c675e
Guilherme Francisco <guilherme.francis2017@gmail.com>
2021-02-24 21:34:18 -0300

    Adicionado ioc do repositório e sua interface.

---
+++ 3/customers/back-end/customers.Infra.CrossCutting/InversionOfControl/RepositoryDependency.cs
@@ -0,0 +1,14 @@
+﻿using customers.Domain.Interfaces;
+using customers.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace customers.Infra.CrossCutting.InversionOfControl
+{
+    public static class RepositoryDependency
+    {
+        public static void AddRepositoryDependency(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryCustomer, CustomerRepository>();
+        }
+    }
+}
