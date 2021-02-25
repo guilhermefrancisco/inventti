46ccf1bd2c3dcf80229a22c1c194c3b1c2da6466
Guilherme Francisco <guilherme.francis2017@gmail.com>
2021-02-24 21:07:30 -0300

    Adicionado BaseRepository, que é responsável por ser a base para qualquer classe de repositório, que fará a manipulação dos dados diretamente no contexto. Criado interface de repositório do cliente.

---
+++ 3/customers/back-end/customers.Domain/Interfaces/IRepositoryCustomer.cs
@@ -0,0 +1,15 @@
+﻿using customers.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace customers.Domain.Interfaces
+{
+    public interface IRepositoryCustomer
+    {
+        void Save(Customer customer);
+        void Remove(int id);
+        Customer GetById(int id);
+        IList<Customer> GetAll();
+    }
+}
---
+++ 3/customers/back-end/customers.Infra.Data/Repository/BaseRepository.cs
@@ -0,0 +1,35 @@
+﻿using customers.Domain.Entities;
+using customers.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace customers.Infra.Data.Repository
+{
+    public class BaseRepository<TEntity, TKeyType> where TEntity : BaseEntity<TKeyType>
+    {
+        protected readonly CustomersContext _customersContext;
+        public BaseRepository(CustomersContext customersContext) => _customersContext = customersContext;
+        protected virtual IList<TEntity> Select() => _customersContext.Set<TEntity>().ToList();
+        protected virtual TEntity Select(int id) => _customersContext.Set<TEntity>().Find(id);
+        protected virtual void Insert(TEntity obj)
+        {
+            _customersContext.Set<TEntity>().Add(obj);
+            _customersContext.SaveChanges();
+        }
+
+        protected virtual void Update(TEntity obj)
+        {
+            _customersContext.Entry(obj).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _customersContext.SaveChanges();
+        }
+
+        protected virtual void Delete(int id)
+        {
+            _customersContext.Set<TEntity>().Remove(Select(id));
+            _customersContext.SaveChanges();
+        }
+
+    }
+}
