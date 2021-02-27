08df76b313278f64629101d1b82e57dae55e5707
Guilherme Francisco <guilherme.francis2017@gmail.com>
2021-02-26 22:19:27 -0300

    Criação da estrutura inicial:    - utils, terá o baseService com as configurações base para acesso a API;    - navigation, contém menu, home e outros possíveis componentes para uso;    - customer, possui o component responsável, dto para manipulação e seu respectivo serviço.

--- 3/customers/front-end/src/app/app-routing.module.ts
+++ 3/customers/front-end/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './customer/component/customer.component';
+import { HomeComponent } from './navigation/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'customers', component: CustomerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- 3/customers/front-end/src/app/app.module.ts
+++ 3/customers/front-end/src/app/app.module.ts
@@ -3,18 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomerComponent } from './customer/customer/customer.component';
+import { CustomerComponent } from './customer/component/customer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MenuComponent } from './navigation/menu/menu.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { HomeComponent } from './navigation/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent
+    CustomerComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
